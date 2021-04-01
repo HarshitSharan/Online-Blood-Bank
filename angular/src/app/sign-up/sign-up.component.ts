a82ad264a5e0e437b6aa59b0ed830287126a4e29
asymptote <harshitsharan2794@gmail.com>
2021-04-01 12:41:44 +0530

    backend connected using localhost COROS added

--- angular/src/app/sign-up/sign-up.component.ts
+++ angular/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router'
 import { StatusReportService } from '../status-report.service';
@@ -13,45 +13,39 @@ export class SignUpComponent implements OnInit {
   obj:any
   constructor(private http: HttpClient ,private route:  Router,private status:StatusReportService) {
     this.Userobj={
-      FirstName:"",
-      LastName:"",
+      firstName:"",
+      fastName:"",
       mobileNumber:undefined,
       email:"",
       active:false,
       password:'',
       role:"User"
     }
-    this.flag=true
+
    }
    sendData()
    {
     console.log(this.Userobj)
-    this.obj={
-      "FirstName" : "fas",
-      "LastName": "afsd",
-      "active": false,
-      "email": "afsd",
-      "mobileNumber": 5635,
-      "password": ""
-      }
-     this.http.post("http://localhost:8080/signup",this.obj).subscribe(data=>
-     {this.flag=data
-      console.log(data)})
-     
-     if(this.flag)
-     {
-      this.status.changeMessage("success")
-        this.route.navigateByUrl('/login')
 
-     }
+     this.http.post("http://localhost:8080/signup",this.Userobj).subscribe(data=>
+     {
+       
+    
+      if(data)
+      {
+        this.status.changeMessage("success")
+          this.route.navigateByUrl('/login')
+
+      }
       else
       {
-        this.status.changeMessage("fail")
-        this.route.navigateByUrl('/login')
+          this.status.changeMessage("fail")
+          this.route.navigateByUrl('/login')
 
       }
-   }
-
+    })
+  
+  }
   ngOnInit(): void {
 
   
--- spring/src/main/java/com/example/controller/SignupController.java
+++ spring/src/main/java/com/example/controller/SignupController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 
 @RequestMapping("/signup")
+@CrossOrigin(origins="*") 
 public class SignupController {
     private final UserRepo userRepo;
 
